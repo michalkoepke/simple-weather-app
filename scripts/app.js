c3c4de999e8c926cacfe3b8c0d011b49fdd6899e
michalkoepke <michalkoepke@gmail.com>
2020-08-08 19:39:07 +0200

    4-proba dodania forecast 5-day - na razie nie dziala.

--- scripts/app.js
+++ scripts/app.js
@@ -3,13 +3,17 @@ const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const detailsRight = document.querySelector('.details-right');
+const detailsRightBottom = document.querySelector('.details-right-bottom');
+
+// console.log(detailsRightBottom);
+// console.log(details);
 
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
 
 const forecast = new Forecast();
 
-console.log(forecast);
+
 
 
 
@@ -23,18 +27,23 @@ console.log(forecast);
 
 const updateUI = (data) => {
 
-    // const cityDetails = data.cityDetails;
-    // const weather = data.weather;
+  // const cityDetails = data.cityDetails;
+  // const weather = data.weather;
+  // const forecasts = data.forecasts;
 
 
-    // destrukruryzxacja properties, czyli to samo co wyzej ale krócej:
+  // destrukruryzxacja properties, czyli to samo co wyzej ale krócej:
 
-    const { cityDetails, weather } = data;
+  const { cityDetails, weather, forecasts } = data;
+
+  // console.log(cityDetails);
+  console.log(forecasts);
+  // console.log(weather);
 
 
-// uaktualniej details template:
+  // uaktualniej details template:
 
-    details.innerHTML = `
+  details.innerHTML = `
 
      <h3 class="my-3 display-4">${cityDetails.EnglishName}</h3>
      <p class="my-3">${weather.LocalObservationDateTime}</p>
@@ -42,14 +51,9 @@ const updateUI = (data) => {
      <div class="my-4">
          <span class="display-4">${weather.Temperature.Metric.Value}</span>
          <span>&deg;C</span>
+      `;
 
-
-
-    
-
-    `;
-
-    detailsRight.innerHTML = `
+  detailsRight.innerHTML = `
 
      
          <div class="my-4">
@@ -60,88 +64,121 @@ const updateUI = (data) => {
            <p class="mb-0">Real feel temp: ${weather.RealFeelTemperatureShade.Metric.Value} degrees</p>
            <p class="mb-0">Cloud cover: ${weather.CloudCover} %</p>
          </div>
+         
+         `;
 
+  detailsRightBottom.innerHTML = `
 
-     </div>
+     
+         
 
-    `;
+         <div class="my-4">
+           <h4>5-day forecast:</h4> 
+        </div>
+           
+        <div class="d-flex">
 
+           <div class="card p-2 bg-light">
+             <p class="mb-4">forecast 1:</p>
+           </div>
 
+           <div class="card p-2 bg-light">
+             <p class="mb-4">forecast 2:</p>
+           </div>
 
+           <div class="card p-2 bg-light">
+             <p class="mb-4">forecast 3:</p>
+           </div>
 
+           <div class="card p-2 bg-light">
+             <p class="mb-4">forecast 3:</p>
+           </div>
 
-
-
-    // uaktualnieniae obrazów dzien i noc oraz ikon:
-
-    // ikony:
-
-    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
-    icon.setAttribute('src', iconSrc);
-    // icon.setAttribute('class', ".icon-big");
-
-
+           <div class="card p-2 bg-light">
+             <p class="mb-4">forecast 3:</p>
+           </div>
+           
+        </div>
     
+  `;
 
 
-    // obraz dzień/noc:
 
-    // wersja lepsza, ternary operator
-
-    // !uwaga
-
-    // let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
-
-    // wersja gorsza, czyli łopatologiczne if:
-
-    // if(weather.IsDayTime){
-    //     timeSrc = 'img/day.svg';
-    // } else {
-    //     timeSrc = 'img/night.svg';
-    // }
-
-    
-
-    // time.setAttribute('src', timeSrc);
-
-    // !koniec
+  // console.log(forecasts.DailyForecasts.Sun.Rise);
 
 
-    // usuń klasę d-none (bootstrapowe display none) jeśli jest obecna:
 
-    if(card.classList.contains('d-none')){
-        card.classList.remove('d-none');
-    };
 
-    console.log(data);
+
+  // uaktualnieniae obrazów dzien i noc oraz ikon:
+
+  // ikony:
+
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+
+  icon.setAttribute('src', iconSrc);
+
+  console.log(iconSrc);
+
+
+  // obraz dzień/noc:
+
+  // wersja lepsza, ternary operator
+
+  // !uwaga
+
+  // let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+
+  // wersja gorsza, czyli łopatologiczne if:
+
+  // if(weather.IsDayTime){
+  //     timeSrc = 'img/day.svg';
+  // } else {
+  //     timeSrc = 'img/night.svg';
+  // }
+
+
+
+  // time.setAttribute('src', timeSrc);
+
+  // !koniec
+
+
+  // usuń klasę d-none (bootstrapowe display none) jeśli jest obecna:
+
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  };
+
+  // console.log(data);
 };
 
 
 
 cityForm.addEventListener('submit', e => {
-    // zapobieganie defaultowej akcji
-    e.preventDefault();
+  // zapobieganie defaultowej akcji
+  e.preventDefault();
 
-    // pobieranie(i przycinanie o spacje przed i po) nazwy miasta
+  // pobieranie(i przycinanie o spacje przed i po) nazwy miasta
 
-    const city = cityForm.city.value.trim();
-    // resetowanie inputu formularza
-    cityForm.reset();
+  const city = cityForm.city.value.trim();
+  // resetowanie inputu formularza
+  cityForm.reset();
 
-    // uaktualnianie UI o nowe miasto
+  // uaktualnianie UI o nowe miasto
 
-    forecast.updateCity(city)
-        .then(data => updateUI(data))
-        .catch(err => console.log(err));
+  forecast.updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
 
 
-    // Set local storage 
+  // Set local storage 
 
-    localStorage.setItem('city', city);
+  localStorage.setItem('city', city);
 });
 
-if(localStorage.getItem('city')){
-    forecast.updateCity(localStorage.getItem('city'))
+if (localStorage.getItem('city')) {
+  forecast.updateCity(localStorage.getItem('city'))
     .then(data => updateUI(data))
     .catch(err => (err));
 }
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,63 +1,89 @@
-class Forecast{
-  constructor(){
-    
+class Forecast {
+  constructor() {
+
     // wlasciwosci
 
 
 
-    this.key = 'LLG0dsysY40EMifWODoOLNpaF83Ed6B6';
+    // this.key = 'LLG0dsysY40EMifWODoOLNpaF83Ed6B6';
+    this.key = 'ms2bRfWWyLViQf0i3ySGGpyxDc3jkAt6';
+
+
 
     this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
     this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search';
-    
+
+    this.forecastURI = 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/';
+
   }
 
 
   // metody
 
-  async updateCity(city){
+  async updateCity(city) {
     const cityDetails = await this.getCity(city);
     const weather = await this.getWeather(cityDetails.Key);
+    const forecasts = await this.getForecast(cityDetails.Key);
 
-    return { cityDetails, weather };
+    return { cityDetails, weather, forecasts };
+
+
 
   }
 
-  async getCity(city){
+  async getCity(city) {
 
     // query - zapytanie o opcjonalne parametry miasta
-    
+
     const query = `?apikey=${this.key}&q=${city}`;
 
     const response = await fetch(this.cityURI + query);
     const data = await response.json();
 
     return data[0];
-    
+
   }
 
-  async getWeather(id){
+  async getWeather(id) {
 
-  const query = `${id}?apikey=${this.key}&details=true`
+    const query = `${id}?apikey=${this.key}&details=true`
 
-  const response = await fetch(this.weatherURI + query);
-  const data = await response.json();
+    const response = await fetch(this.weatherURI + query);
+    const data = await response.json();
+    // console.log(query);
 
-  return data[0];
+
+    console.log(query);
+    console.log(data);
+
+    return data[0];
 
   }
 
 
   // !experimental stuff - forecast na kilka dni
 
+  async getForecast(id) {
+
+
+    const query = `858642?apikey=${this.key}`
+
+    const response = await fetch(this.forecastURI + query);
+    const data = await response.json();
+    // console.log(query);
+    // console.log(data);
+    
+    return data;
+    
+
+    // ! ponizej poprawne zapytanie ze stronki accuweather:
+
+    // GET /forecasts/v1/daily/5day/858642?apikey=TtZ3JFrGXeWF2GKZXy0aOaN2sB1ibzB8 HTTP/1.1
+
+  }
 
 
 
+}
 
 
-
-
-} 
-
-
-  
